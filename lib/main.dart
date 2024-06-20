daf44407c287e564e0c155e2e0918cbe2b318ca7
Khaled Mohaseb <khaledmohaseb319@gmail.com>
2024-06-21 00:56:58 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:bookly/Features/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const booklyapp());
+}
+
+class booklyapp extends StatelessWidget {
+  const booklyapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: splashview(),
+
+    );
+  }
+}
