cb3c87724837bdf6480e68094d6443a52a03117d
Khaled Mohaseb <khaledmohaseb319@gmail.com>
2024-07-15 01:49:56 +0300

    add  Cached Network Image

--- lib/Features/home/data/presentation/views/widgets/home widgets/Custom_Book_Item.dart
+++ lib/Features/home/data/presentation/views/widgets/home widgets/Custom_Book_Item.dart
@@ -1,25 +1,26 @@
 // ignore: file_names
 
-
+import 'package:bookly/core/widgets/loading.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookItem extends StatelessWidget {
   const CustomBookItem({super.key, required this.imageurl});
- final String imageurl;
+  final String imageurl;
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
-      aspectRatio: 2 / 3.1,
-      child: Container(
-        decoration:  BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(12)),
-        
-          image: DecorationImage(
+        aspectRatio: 2 / 3.1,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          child: CachedNetworkImage(
+            imageUrl: '',
             fit: BoxFit.fill,
-            image: NetworkImage(imageurl),
+            errorWidget: (context, url, error) => const Icon(Icons.error),
+            placeholder: (context, url) => const LoadingIndicator()
+            ),
           ),
-        ),
-      ),
-    );
+        );
   }
 }
