cbdd20ca65dd12a1bed289f911110b01c0309bb3
Khaled Mohaseb <khaledmohaseb319@gmail.com>
2024-07-30 01:29:02 +0300

    done

--- lib/Features/search/presentation/view/search_page.dart
+++ lib/Features/search/presentation/view/search_page.dart
@@ -1,11 +1,7 @@
-
-import 'package:bookly/Features/search/presentation/manger/searchbbook/searchbook_cubit.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-
+import '../manger/searchbbook/searchbook_cubit.dart';
 import 'widgets/best_seller_book_search.dart';
 import 'widgets/search_textfield.dart';
 
@@ -17,39 +13,40 @@ class SearchBage extends StatefulWidget {
 }
 
 class _SearchBageState extends State<SearchBage> {
-  String? text;
-   GlobalKey<FormState> formKey = GlobalKey();
-  @override
+  final TextEditingController _searchController = TextEditingController();
+  GlobalKey<FormState> formKey = GlobalKey();
+
+  @override  
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body:  Form(
+    return Scaffold(
+      body: Form(
         key: formKey,
         child: Padding(
-          padding:const EdgeInsets.symmetric(horizontal: 20 , ),
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             children: [
-              SearchTextField( onFieldSubmitted: (value) {
-                if (formKey.currentState!.validate()) {
-  text=value;
-  setState(() {
-    BlocProvider.of<SearchbookCubit>(context).fetchSearchBooks(searchPoint: text!);
-  });
-}
-              },),
-              const SizedBox(
-                height: 50,
+              SearchTextField(
+                controller: _searchController,
+                onChanged: (value) {
+                  BlocProvider.of<SearchbookCubit>(context).fetchSearchBooks(searchPoint: value);
+                },
+                onFieldSubmitted: (value) {
+                  if (formKey.currentState!.validate()) {
+                    BlocProvider.of<SearchbookCubit>(context).fetchSearchBooks(searchPoint: value);
+                    _searchController.clear();
+                  }
+                },
               ),
-             const  Align(
+              const SizedBox(height: 50),
+              const Align(
                 alignment: Alignment.centerLeft,
                 child: Text(
                   'Search Results',
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
               ),
-              const SizedBox(
-                height: 20,
-              ),
-            const  Expanded(child:  BestSellerBooksSearch())
+              const SizedBox(height: 20),
+              const Expanded(child: BestSellerBooksSearch()),
             ],
           ),
         ),
--- lib/Features/search/presentation/view/widgets/search_textfield.dart
+++ lib/Features/search/presentation/view/widgets/search_textfield.dart
@@ -2,31 +2,39 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SearchTextField extends StatelessWidget {
-  const SearchTextField({super.key, this.onFieldSubmitted});
+  final TextEditingController controller;
   final void Function(String value)? onFieldSubmitted;
+  final void Function(String value)? onChanged;
+
+  const SearchTextField({
+    Key? key,
+    required this.controller,
+    this.onFieldSubmitted,
+    this.onChanged,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-      padding:const  EdgeInsets.symmetric(horizontal: 0,),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 0),
       child: SafeArea(
         child: TextFormField(
+          controller: controller,
           validator: (value) {
             if (value!.isEmpty) {
-              return 'Please enter the name of the book ';
+              return 'Please enter the name of the book';
             }
             return null;
           },
-          
           onFieldSubmitted: onFieldSubmitted,
+          onChanged: onChanged,
           decoration: const InputDecoration(
             suffixIcon: Icon(CupertinoIcons.search),
-            label: Text("seach"),
-            hintText: 'enter book name',
-
-        border: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.white)
-        )
+            label: Text("Search"),
+            hintText: 'Enter book name',
+            border: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
           ),
         ),
       ),
